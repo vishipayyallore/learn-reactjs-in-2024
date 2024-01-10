d88d916dbbee30a2a7faabb3492d84ff398f9143
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-01-10 15:27:21 +0530

    Reorganization

--- todo-njs/src/app/layout.jsx
+++ todo-njs/src/app/layout.jsx
@@ -1,6 +1,6 @@
 import { Inter } from 'next/font/google'
 import '@/styles/globals.css'
-import Header from '@/app/components/nav/Header'
+import Header from '@/app/components/shared/Header'
 
 const inter = Inter({ subsets: ['latin'] })
 
